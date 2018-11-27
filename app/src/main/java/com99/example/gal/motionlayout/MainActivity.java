82844cad01315b6c9efe23829a034d1b9d0fc6c4
Gal Maoz <galm@gett.com>
2018-11-27 16:39:57 +0200

    clean code

--- app/src/main/java/com99/example/gal/motionlayout/MainActivity.java
+++ app/src/main/java/com99/example/gal/motionlayout/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         animateToStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mMotionLayout.transitionToStart();
+
             }
         });
     }
